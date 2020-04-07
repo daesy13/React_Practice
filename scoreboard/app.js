494049aae4900cc15f7e79fad79de7ab4854daed
Daesy <stedaesy@amazon.com>
2020-04-06 23:23:59 -0700

    init scoreboard

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,26 +1,3 @@
-const players = [
-    {
-        name: "Daesy",
-        score: 50,
-        id: 1
-    },
-    {
-        name: "Sue",
-        score: 90,
-        id: 2
-    },
-    {
-        name: "Ann",
-        score: 85,
-        id: 3
-    },
-    {
-        name: "Tina",
-        score: 80,
-        id: 4
-    }
-];
-
 const Header = (props) => {
     return (
         <header>
@@ -34,45 +11,106 @@ const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
+                <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>✖</button>
                 {props.name}
             </span>
-            <Counter score={props.score} />
+            <Counter />
         </div>
     );
 }
 
-const Counter = (props) => {
-    return (
-        <div className="counter">
-            <button className="counter-action decrement"> - </button>
-            <span className="counter-score">{ props.score }</span>
-            <button className="counter-action increment"> + </button>
-        </div>
-    );
+// *** IN ORDER TO USE AN STATE WE NEED TO CONVERT OUR COMPONENT INTO A CLASS ***
+
+class Counter extends React.Component {
+    state = {
+        score: 0
+    };
+
+    incrementScore= () => {
+        // setState will update the value and tell react to render the method
+        this.setState( prevState => ({
+            score: this.state.score + 1
+        }));
+    }
+
+    decrementScore = () => {
+        this.setState( prevState => ({
+            score: this.state.score - 1
+        }));
+    }
+
+    render() {
+        return (
+            <div className="counter">
+            {/* YOU CAN USE AN ARROW FUNCTION OR BIND FOR INCREMENT OR DECREMENT*/}
+                <button className="counter-action decrement" onClick={ this.decrementScore }> - </button>
+                <span className="counter-score">{ this.state.score }</span>
+                <button className="counter-action increment" onClick={ this.incrementScore }> + </button>
+            </div>
+        );
+    }
 }
 
-const App = (props) => {
-    return (
-        <div className="scoreboard">
-            <Header
-            title="Scoreboard"
-            totalPlayers={props.initialPlayers.length}
-            />
 
-            {/* Players list */}
-            {props.initialPlayers.map( player =>
-                <Player
-                    name={player.name}
-                    score={player.score}
-                    key={player.id.toString()}
+// THIS IS A COMPONENT
+// *** IN ORDER TO USE AN STATE WE NEED TO CONVERT OUR COMPONENT INTO A CLASS ***
+
+class App extends React.Component {
+    state = {
+        players: [
+            {
+                name: "Daesy",
+                id: 1
+            },
+            {
+                name: "Sue",
+                id: 2
+            },
+            {
+                name: "Ann",
+                id: 3
+            },
+            {
+                name: "Tina",
+                id: 4
+            }
+        ]
+    }
+
+    handleRemovePlayer = (id) => {
+        this.setState ( prevState => {
+            return {    
+                // return all players whos id doesn't match the one clicked
+                players: this.state.players.filter( p => p.id !== id )
+            }
+        });
+    }
+
+    render(){
+        return (
+            <div className="scoreboard">
+                <Header
+                title="Scoreboard"
+                totalPlayers={this.state.players.length}
                 />
-            )}
-        </div>
-    );
+
+                {/* Players list */}
+                {this.state.players.map( player =>
+                    <Player
+                        name={player.name}
+                        id={player.id}
+                        key={player.id.toString()}
+                        removePlayer={this.handleRemovePlayer}
+                    />
+                )}
+            </div>
+        );
+    }
 }
+
 
 ReactDOM.render(
     // the closing tag close that way when it doesn't have children & req the extra space
-    <App initialPlayers={ players } />,
+    <App />,
     document.getElementById('root')
 );
